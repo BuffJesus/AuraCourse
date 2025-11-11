8f3d594037a6424fd7a5cb1d6f4867c5a56c4697
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2025-11-10 20:18:47 -0700

    Implemented some attributes. Still need to utilize ATTRIBUTE_ACCESSORS_BASIC

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -13,4 +13,38 @@ UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	UAuraAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+#pragma region VitalAttributes	
+#pragma region Health
+	UPROPERTY(BlueprintReadOnly, Category = "Aura|VitalAttributes", ReplicatedUsing = OnRep_Health)
+	FGameplayAttributeData Health;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Aura|VitalAttributes", ReplicatedUsing = OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+#pragma endregion	
+#pragma region Mana
+	UPROPERTY(BlueprintReadOnly, Category = "Aura|VitalAttributes", ReplicatedUsing = OnRep_Mana)
+	FGameplayAttributeData Mana;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Aura|VitalAttributes", ReplicatedUsing = OnRep_MaxMana)
+	FGameplayAttributeData MaxMana;
+
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+#pragma endregion
+#pragma endregion	
+	
 };
