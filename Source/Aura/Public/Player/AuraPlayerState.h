e00063b2c78120235cbbd1b522cedb08e2efa4d9
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2025-11-10 22:39:22 -0700

    I gotta stop listening to AI.

--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -7,6 +7,7 @@
 #include "GameFramework/PlayerState.h"
 #include "AuraPlayerState.generated.h"
 
+class UAuraAbilitySystemComponent;
 class UAbilitySystemComponent;
 class UAuraAttributeSet;
 
@@ -22,12 +23,12 @@ public:
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 	
 	// Typed accessors
-	UAbilitySystemComponent* GetAuraASC() const { return AbilitySystemComponent; }
+	UAuraAbilitySystemComponent* GetAuraASC() const { return AbilitySystemComponent; }
 	UAuraAttributeSet* GetAttributeSet() const { return AttributeSet; }
 
 protected:
 	UPROPERTY(VisibleAnywhere, Category = "Aura|GAS")
-	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+	TObjectPtr<UAuraAbilitySystemComponent> AbilitySystemComponent;
 
 	UPROPERTY()
 	TObjectPtr<UAuraAttributeSet> AttributeSet;
