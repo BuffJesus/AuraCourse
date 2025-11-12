949be19aea23a616dc57be04db93ad7c415d4f6c
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2025-11-11 20:22:13 -0700

    Added mana delegates etc to overlay widget controller.

--- Source/Aura/Private/UI/Controllers/AuraOverlayWidgetController.cpp
+++ Source/Aura/Private/UI/Controllers/AuraOverlayWidgetController.cpp
@@ -9,9 +9,13 @@ void UAuraOverlayWidgetController::BroadcastInitialValues()
 	const UAuraAttributeSet* AuraAttributeSet = CastChecked<UAuraAttributeSet>(AttributeSet);
 	const auto Health = AuraAttributeSet->GetHealth();
 	const auto MaxHealth = AuraAttributeSet->GetMaxHealth();
+	const auto Mana = AuraAttributeSet->GetMana();
+	const auto MaxMana = AuraAttributeSet->GetMaxMana();
 
 	OnHealthChanged.Broadcast(Health);
 	OnMaxHealthChanged.Broadcast(MaxHealth);
+	OnManaChanged.Broadcast(Mana);
+	OnMaxManaChanged.Broadcast(MaxMana);
 }
 
 void UAuraOverlayWidgetController::BindCallbacksToDependencies()
@@ -23,6 +27,12 @@ void UAuraOverlayWidgetController::BindCallbacksToDependencies()
 	
 	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
 		AuraAttributeSet->GetMaxHealthAttribute()).AddUObject(this, &UAuraOverlayWidgetController::MaxHealthChanged);
+
+	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
+		AuraAttributeSet->GetManaAttribute()).AddUObject(this, &UAuraOverlayWidgetController::ManaChanged);
+
+	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
+		AuraAttributeSet->GetMaxManaAttribute()).AddUObject(this, &UAuraOverlayWidgetController::MaxManaChanged);
 }
 
 void UAuraOverlayWidgetController::HealthChanged(const FOnAttributeChangeData& Data) const
@@ -33,4 +43,14 @@ void UAuraOverlayWidgetController::HealthChanged(const FOnAttributeChangeData& D
 void UAuraOverlayWidgetController::MaxHealthChanged(const FOnAttributeChangeData& Data) const
 {
 	OnMaxHealthChanged.Broadcast(Data.NewValue);
+}
+
+void UAuraOverlayWidgetController::ManaChanged(const FOnAttributeChangeData& Data) const
+{
+	OnManaChanged.Broadcast(Data.NewValue);
+}
+
+void UAuraOverlayWidgetController::MaxManaChanged(const FOnAttributeChangeData& Data) const
+{
+	OnMaxManaChanged.Broadcast(Data.NewValue);
 }
