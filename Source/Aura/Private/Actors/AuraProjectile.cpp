0ec3d12ec53afd73f2f14c4ba62edf2b5e5ac495
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2025-11-24 16:20:00 -0700

    Refactored impact effects in `AuraProjectile` by introducing `PlayImpactEffects` to avoid duplication and improve readability.

--- Source/Aura/Private/Actors/AuraProjectile.cpp
+++ Source/Aura/Private/Actors/AuraProjectile.cpp
@@ -46,28 +46,33 @@ void AAuraProjectile::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	SetLifeSpan(10.f);
+	SetLifeSpan(LifeSpan);
 	
 	// Only authoritative projectiles handle overlaps
 	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraProjectile::OnSphereOverlap);
+}
+
+void AAuraProjectile::PlayImpactEffects() const
+{
+	UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator);
+	UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation());
 }
 
 void AAuraProjectile::Destroyed()
 {
 	if (!bHit && !HasAuthority())
 	{
-		UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator);
-		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation());
+		PlayImpactEffects();
 	}
 	Super::Destroyed();
 }
 
 void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-                                      UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+									  UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator);
-	UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ImpactEffect, GetActorLocation());
+	PlayImpactEffects();
 	
 	if (HasAuthority()) { Destroy(); }
 	else { bHit = true; }
 }
+
--- Source/Aura/Public/Actors/AuraProjectile.h
+++ Source/Aura/Public/Actors/AuraProjectile.h
@@ -32,6 +32,7 @@ protected:
 	UFUNCTION()
 	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, 
 		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	void PlayImpactEffects() const;
 
 private:
 	bool bHit { false };
