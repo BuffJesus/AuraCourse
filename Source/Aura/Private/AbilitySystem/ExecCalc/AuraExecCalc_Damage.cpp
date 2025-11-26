4e64ce93d601f52847e0efc52f0b19ee5bfbc60b
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2025-11-25 21:41:22 -0700

    Added armor attribute capture to `UAuraExecCalc_Damage` for improved damage calculation support.

--- Source/Aura/Private/AbilitySystem/ExecCalc/AuraExecCalc_Damage.cpp
+++ Source/Aura/Private/AbilitySystem/ExecCalc/AuraExecCalc_Damage.cpp
@@ -3,10 +3,26 @@
 
 #include "AbilitySystem/ExecCalc/AuraExecCalc_Damage.h"
 #include "AbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+struct AuraDamageStatics
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	AuraDamageStatics() : ArmorProperty(nullptr)
+	{
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target, false);
+	}
+};
+
+static const AuraDamageStatics& DamageStatics()
+{
+	static const AuraDamageStatics DamageStatics;
+	return DamageStatics;
+}
 
 UAuraExecCalc_Damage::UAuraExecCalc_Damage()
 {
-	
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
 }
 
 void UAuraExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
