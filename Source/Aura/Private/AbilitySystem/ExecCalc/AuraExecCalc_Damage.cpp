cb85ea56d30ccaf39723f04cf3f6905e5d9921ae
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2025-11-25 21:51:52 -0700

    Added gameplay tag support to `UAuraExecCalc_Damage` and refined armor attribute calculation.

--- Source/Aura/Private/AbilitySystem/ExecCalc/AuraExecCalc_Damage.cpp
+++ Source/Aura/Private/AbilitySystem/ExecCalc/AuraExecCalc_Damage.cpp
@@ -37,4 +37,19 @@ void UAuraExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExe
     const AActor* TargetAvatar { GetAvatarFromASC(ExecutionParams.GetTargetAbilitySystemComponent()) };
 	
 	const FGameplayEffectSpec Spec { ExecutionParams.GetOwningSpec() };
+	
+	const FGameplayTagContainer* SourceTags { Spec.CapturedSourceTags.GetAggregatedTags() };
+	const FGameplayTagContainer* TargetTags { Spec.CapturedTargetTags.GetAggregatedTags() };
+	
+	FAggregatorEvaluateParameters EvalParams;
+	EvalParams.SourceTags = SourceTags;
+	EvalParams.TargetTags = TargetTags;
+	
+	float Armor { 0.f };
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvalParams, Armor);
+	Armor = FMath::Max<float>(0.f, Armor);
+	++Armor;
+
+	const FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor);
+	OutExecutionOutput.AddOutputModifier(EvaluatedData);
 }
