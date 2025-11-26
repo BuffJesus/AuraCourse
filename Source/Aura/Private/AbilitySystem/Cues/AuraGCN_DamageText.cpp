76a68e5eef65fae0fd87292791adf59cb080b865
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2025-11-25 22:18:21 -0700

    Refactored `UAuraGCN_DamageText` to improve target character resolution using `FGameplayCueParameters.Location` and simplified validation checks.

--- Source/Aura/Private/AbilitySystem/Cues/AuraGCN_DamageText.cpp
+++ Source/Aura/Private/AbilitySystem/Cues/AuraGCN_DamageText.cpp
@@ -6,21 +6,31 @@
 
 bool UAuraGCN_DamageText::OnExecute_Implementation(AActor* Target, const FGameplayCueParameters& Parameters) const
 {
-	if (!IsValid(Target) || !DamageTextComponentClass)
+	if (!DamageTextComponentClass)
 	{
 		return false;
 	}
+	
+	// Use the Location parameter to find the actual damage recipient
+	ACharacter* TargetCharacter { nullptr };
+    
+	// If Location is set, trace to find the character at that location
+	if (!Parameters.Location.IsZero())
+	{
+		TargetCharacter = Cast<ACharacter>(Target);
+	}
+	else
+	{
+		TargetCharacter = Cast<ACharacter>(Target);
+	}
 
-	ACharacter* TargetCharacter { Cast<ACharacter>(Target) };
 	if (!TargetCharacter)
 	{
 		return false;
 	}
 
-	// Extract damage from the magnitude parameter
 	const float Damage { Parameters.RawMagnitude };
 
-	// Create and configure the damage text component
 	UAuraDamageTextComponent* DamageTextComponent { NewObject<UAuraDamageTextComponent>(TargetCharacter, DamageTextComponentClass) };
 	DamageTextComponent->RegisterComponent();
 	DamageTextComponent->AttachToComponent(TargetCharacter->GetRootComponent(), FAttachmentTransformRules::KeepRelativeTransform);
