55afc68716e547332b8c4f31d5d0f0d95cecc645
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2025-11-26 20:38:49 -0700

    Added `AuraAbilitySystemGlobals` with custom effect context allocation and updated project configuration settings.

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,10 @@
+// Not Sure Yet
+
+
+#include "AbilitySystem/AuraAbilitySystemGlobals.h"
+#include "AuraAbilityTypes.h"
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FAuraGameplayEffectContext();
+}
